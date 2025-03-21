6830a91561b7c010ec583429d2f21a3695bb0529
DongHyeon Ryu <ydh9516@gmail.com>
2025-03-21 19:19:49 +0900

    init

---
+++ 12100.py
@@ -0,0 +1,108 @@
+from copy import deepcopy
+from pprint import pprint
+
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+LEFT, RIGHT, UP, DOWN = (0,-1),(0,1),(-1,0),(1,0)
+directions = [LEFT, RIGHT, UP, DOWN]
+
+def get_max(graph)->int:
+    v = -1
+    for row in graph:
+        for e in row:
+            v= max(v, e)
+    return v
+
+def transpose(graph):
+    return list([*tup] for tup in zip(*graph))
+
+def flip(graph):
+    for row in graph:
+        row.reverse()
+
+def _compress(row):
+    n = len(row)
+    left = 0
+    right = 1
+
+    while left < n and right < n:
+        # print(row, left, right)
+        if row[left] == 0:
+            left += 1
+            continue
+        if row[right] == 0:
+            right += 1
+            continue
+        if left >= right:
+            right += 1
+            continue
+        
+
+        if row[left] == row[right]:
+            row[left] *= 2
+            row[right] = 0
+            left = right+1
+            right += 2
+        else:
+            left = right
+            right += 1
+
+    # print(row, left, right)
+    last_v = -1
+    for i in range(n):
+        if row[i] == 0:
+            continue
+        else:
+            last_v += 1 
+            row[last_v] = row[i]
+            if i != last_v:
+                row[i] = 0
+    # print(row)
+
+
+def _move_left(graph):
+    for row in graph:
+        _compress(row)
+
+def move(graph, direction):
+    if direction == LEFT:
+        _move_left(graph)
+    elif direction == UP:
+        _move_left(graph := transpose(graph))
+        graph = transpose(graph)
+    elif direction == RIGHT:
+        flip(graph)
+        _move_left(graph)
+        flip(graph)
+    elif direction == DOWN:
+        flip(graph:=transpose(graph))
+        _move_left(graph)
+        flip(graph)
+        graph = transpose(graph)
+
+    return graph
+
+        
+def dfs(graph, cnt, direction)->int:
+    # print(f"{cnt}, {direction} (before)")
+    # pprint(graph)
+    # print()
+    graph = move(graph, direction)
+    # print(f"{cnt}, {direction} (after)")
+    # pprint(graph, width=20)
+    if cnt == 5:
+        return get_max(graph)
+    
+    ans = -1
+    for _direc in directions:
+        ans = max(ans, dfs(deepcopy(graph), cnt+1, _direc))
+
+    return ans
+
+ans = -1
+for _direc in directions:
+    ans = max(ans, dfs(deepcopy(graph), 1, _direc))
+
+print(ans)
